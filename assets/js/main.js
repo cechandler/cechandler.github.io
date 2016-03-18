dc0eb4f8b8f1d51a7feaaae488bdcaca1465702d
Chris Chandler <christopher.e.chandler@gmail.com>
2016-03-18 10:47:55 -0400

    random background chooser
    javascript to randomly choose from a list of body background colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,8 @@ $(function(){
   });
 });
 
-// FANCYBOX
-// $(document).ready(function() {
-// 	$(".fancybox").fancybox();
-// });
+// RANDOM BACKGROUND COLOR CHOOSER
+$(document).ready(function(){
+  var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
+  $('body').css({'background': '' + colors[Math.floor(Math.random() * colors.length)]});
+});
