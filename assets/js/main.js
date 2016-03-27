9937bf089267127cdc92b6db361077f6b684eef8
Chris Chandler <christopher.e.chandler@gmail.com>
2016-03-27 10:45:09 -0400

    link adjustment and color removal
    - adjusted links so .html isn’t displayed
    - removed random background color

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ $(function(){
 });
 
 // RANDOM BACKGROUND COLOR CHOOSER
-$(document).ready(function(){
-  var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
-  $('body').css({'background': '' + colors[Math.floor(Math.random() * colors.length)]});
-});
+// $(document).ready(function(){
+//   var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
+//   $('body').css({'background': '' + colors[Math.floor(Math.random() * colors.length)]});
+// });
